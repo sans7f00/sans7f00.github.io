3e896467e65c5db5ca45a7b863e282f45783183b
sans7f00 <sansundertale-666@hotmail.com>
2024-07-05 20:38:28 +1200

    meh

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-// stuff
-brainCellCount = random(-10 ** 12, 10 ** 12);
+// randomness
+brainCellCount = Math.floor(Math.random() * 10 ** 12 - Math.random() * 10 ** 12);
 document.getElementById("braincells").innerHTML = brainCellCount;
