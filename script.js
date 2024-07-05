b42791bdbbc3825f80b2027c32cd77e6bc46c53a
sans7f00 <sansundertale-666@hotmail.com>
2024-07-05 20:58:58 +1200

    setInterval

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // randomness
 brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecision(3);
+  setInterval(() {
+  if (brainCellCount < 0) {
+    brainCellCount = -(Math.abs(brainCellCount) ** 1.01);
+  } else if (brainCellCount = 0) {
+    brainCellCount++;
+    brainCellCount ** 1.01;
+  } else {
+    brainCellCount **= 1.01;
+  }
+}, 50);
 document.getElementById("braincells").innerHTML = brainCellCount;
