c8f37ccd3f795859885818219ececce0841b611f
sans7f00 <sansundertale-666@hotmail.com>
2024-07-05 21:32:28 +1200

    nah

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // randomness
 brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecision(3);
-setInterval(() => {
-  if (brainCellCount < 0) {
+if (brainCellCount < 0) {
+  setInterval(() => {
     brainCellCount = -(Math.abs(brainCellCount) ** 1.01);
-  } else if (brainCellCount = 0) { // highly unlikely
-    brainCellCount++;
+  }, 50);
+} else if (brainCellCount = 0) { // highly unlikely
+  brainCellCount = 1;
+} else {
+  setInterval(() => {
     brainCellCount **= 1.01;
-  } else {
-    brainCellCount **= 1.01;
-  }
-}, 50);
+  }, 50);
+}
 document.getElementById("braincells").innerHTML = brainCellCount;
