4aa919d25387c30d2e5a5c2734bdf38d419381e7
sans7f00 <sansundertale-666@hotmail.com>
2024-07-06 13:39:36 +1200

    Debug?

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // randomness
 brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecision(3);
-// WARNING! probably faulty code
+// WARNING! code might not work
 if (brainCellCount < 0) {
   setInterval(() => {
     brainCellCount = (Math.abs(brainCellCount) ** 1.01) * -1;
+    document.getElementById("braincells").innerHTML = brainCellCount;
   }, 50);
 } else if (brainCellCount = 0) { // highly unlikely
   brainCellCount = 1;
 } else {
   setInterval(() => {
     brainCellCount **= 1.01;
+    document.getElementById("braincells").innerHTML = brainCellCount;
   }, 50);
 }
 document.getElementById("braincells").innerHTML = brainCellCount;
