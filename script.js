7332561c3b83a9d237b0fcdd6a1acef183db0679
sans7f00 <sansundertale-666@hotmail.com>
2024-07-06 13:41:49 +1200

    Fix

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecisi
 if (brainCellCount < 0) {
   setInterval(() => {
     brainCellCount = (Math.abs(brainCellCount) ** 1.01) * -1;
-    document.getElementById("braincells").innerHTML = brainCellCount;
+    document.getElementById("braincells").innerHTML = brainCellCount.toPrecision(3);
   }, 50);
 } else if (brainCellCount = 0) { // highly unlikely
   brainCellCount = 1;
 } else {
   setInterval(() => {
     brainCellCount **= 1.01;
-    document.getElementById("braincells").innerHTML = brainCellCount;
+    document.getElementById("braincells").innerHTML = brainCellCount.toPrecision(3);
   }, 50);
 }
 document.getElementById("braincells").innerHTML = brainCellCount;
