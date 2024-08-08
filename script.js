3e4fe831be601384d4ef2a786256ae0185f584ec
sans7f00 <sansundertale-666@hotmail.com>
2024-08-09 08:49:33 +1200

    Oh well

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // WARNING! might be faulty
 brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecision(3);
-if (brainCellCount <= 0) {
+if (brainCellCount < 0) {
   setInterval(() => {
     brainCount--;
     brainCellCount = (Math.abs(brainCellCount) ** 1.01) * -1;
-    iq = (Math.abs(brainCellCount) ** 0.09) * -1;
+    iq = ((Math.abs(brainCellCount) ** 0.09) * -1).toPrecision(3);
     document.getElementById("braincells").innerHTML = brainCellCount.toPrecision(3);
     document.getElementById("iq").innerHTML = iq.toPrecision(3);
   }, 50);
+} else if (brainCellCount = 0) {
+  document.getElementById("braincells").innerHTML = 0;
+  document.getElementById("iq").innerHTML = 0;
 } else {
   setInterval(() => {
     brainCellCount **= 1.01;
-    iq = brainCellCount ** 0.09;
+    iq = (brainCellCount ** 0.09).toPrecision(3);
     document.getElementById("braincells").innerHTML = brainCellCount.toPrecision(3);
-    document.getElementById("iq").innerHTML = iq.toPrecision(3)
+    document.getElementById("iq").innerHTML = iq.toPrecision(3);
   }, 50);
 }
