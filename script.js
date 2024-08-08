e9f1df163dd730d3183384fdec123721939eae4d
sans7f00 <sansundertale-666@hotmail.com>
2024-08-09 09:01:25 +1200

    i have no idea

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecision(3);
 if (brainCellCount < 0) {
   setInterval(() => {
-    brainCount--;
     brainCellCount = (Math.abs(brainCellCount) ** 1.01) * -1;
     iq = ((Math.abs(brainCellCount) ** 0.09) * -1).toPrecision(3);
     document.getElementById("braincells").innerHTML = brainCellCount.toPrecision(3);
