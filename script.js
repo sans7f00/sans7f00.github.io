5f45b186ff4c1b8edbe3b205af328819679df187
sans7f00 <sansundertale-666@hotmail.com>
2024-08-11 15:49:35 +1200

    Added some more comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // WARNING! might be faulty
 brainCellCount = (Math.random() * 10 ** 12 - Math.random() * 10 ** 12).toPrecision(3);
 if (brainCellCount < 0) {
+  // this happens every 50 ms
   setInterval(() => {
     brainCellCount = (Math.abs(brainCellCount) ** 1.01) * -1;
     iq = (Math.abs(brainCellCount) ** 0.09).toPrecision(3) * -1;
@@ -8,6 +9,8 @@ if (brainCellCount < 0) {
     document.getElementById("iq").innerHTML = iq.toPrecision(3);
   }, 50);
 } else {
+  /* this happens every 25 seconds, but the brain cell growth is linear compared to exponential when brainCellCount is negative
+  so you would have not much brain cells */
   setInterval(() => {
     brainCellCount++;
     iq = (brainCellCount ** 0.09).toPrecision(3);
@@ -15,5 +18,3 @@ if (brainCellCount < 0) {
     document.getElementById("iq").innerHTML = iq.toPrecision(3);
   }, 25);
 }
-
-sans = createImg('https://raw.githubusercontent.com/sans7f00/sans7f00.github.io/main/sans.jpeg', '...');
